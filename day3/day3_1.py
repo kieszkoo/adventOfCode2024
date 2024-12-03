fc3e15f7595383093d438a96e185017e3c52e759
kieszkoo <kiemic13@gmail.com>
2024-12-03 18:07:16 +0100

    day 3 part 1

---
+++ day3/day3_1.py
@@ -0,0 +1,21 @@
+import re
+
+def mul(x, y):
+    return x * y
+
+pattern = r"mul[(][0-9]{1,3},[0-9]{1,3}[)]"
+functions = []
+sum = 0
+
+with open("day3.txt", "r") as file:
+    for line in file:
+        match = re.findall(pattern, line)
+        functions.append(match)
+
+for i in functions:
+    for function in i:
+        print(function)
+        print(eval(function))
+        sum += eval(function)
+
+print(sum)
