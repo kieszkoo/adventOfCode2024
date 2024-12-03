cbcddd97d9c8e4165df46c10d1b054d13e470224
kieszkoo <kiemic13@gmail.com>
2024-12-03 17:44:30 +0100

    day 2 part 2

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -3,6 +3,16 @@ def is_safe(report):
     decreasing = all(1 <= report[i] - report[i + 1] <= 3 for i in range(len(report) - 1))
     return increasing or decreasing
 
+def is_safe_with_removal(report):
+    if is_safe(report):
+        return True
+    for i in range(len(report)):
+        modified_report = report[:i] + report[i+1:]
+        if is_safe(modified_report):
+            return True
+    return False
+
+
 dane=[]
 with open("day2.txt", "r") as file:
     for line in file:
@@ -10,5 +20,5 @@ with open("day2.txt", "r") as file:
         int_row = list(map(int, row))
         dane.append(int_row)
 
-safe_reports_count = sum(1 for report in dane if is_safe(report))
+safe_reports_count = sum(1 for report in dane if is_safe_with_removal(report))
 print(safe_reports_count)
